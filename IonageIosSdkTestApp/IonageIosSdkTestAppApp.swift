d2808ad48265be190c6897f61b9dee527c8edb1a
Binay Shaw <binay.s@ionage.in>
2024-10-20 16:12:34 +0530

    Initial Commit

---
+++ IonageIosSdkTestApp/IonageIosSdkTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IonageIosSdkTestAppApp.swift
+//  IonageIosSdkTestApp
+//
+//  Created by Binay Shaw on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IonageIosSdkTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
